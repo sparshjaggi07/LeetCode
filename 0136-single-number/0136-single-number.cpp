b2cbaa6aca720fface9782b81e995733dae4636f
Sparsh Jaggi <sparshjaggi2607@gmail.com>
2025-07-18 18:46:15 +0530

    Time: 12 ms (12.97%), Space: 24 MB (9.12%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,19 @@
+#include <unordered_map>
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map <int,int> mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        }
+        for(int j=0;j<nums.size();j++)
+        {
+            if(mp[nums[j]]==1)
+            {
+                return nums[j];
+            }
+        }
+        return -1;
+    }
+};
