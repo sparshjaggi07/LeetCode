51439975e27a56dd844760466f13949af90fda1f
Sparsh Jaggi <sparshjaggi2607@gmail.com>
2025-07-18 19:16:00 +0530

    Time: 0 ms (100%), Space: 11.8 MB (11.98%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-    int helper(vector<int>& nums,int low,int high){
-        int pivot=nums[low];
-        int i=low,j=high;
-        while(i<j){
-            while(nums[i]<=pivot && i<=high-1){
-                i++;
-            }
-            while(nums[j]>pivot && j>=low+1){
-                j--;
-            }
-            if(i<j)swap(nums[i],nums[j]);
-        }
-        swap(nums[low],nums[j]);
-        return j;
-    }
-    void qs(vector<int>& nums,int low,int high) {
-        int pi=0;
-        if(low<high){
-            pi=helper(nums,low,high);
-            qs(nums,low,pi-1);
-            qs(nums,pi+1,high);
-        }
-        }
         void sortColors(vector<int>& nums) {
-            int low=0;int high=nums.size()-1;
-            qs(nums,low,high);
+            unordered_map<int,int> count;
+            for(int i=0;i<nums.size();i++){
+                count[nums[i]]++;
+            }
+            int ind=0;
+            for(int i=0;i<3;i++){
+                while(count[i]>0){
+                    nums[ind]=i;
+                    ind++;
+                    count[i]--;
+                }
+                count[i]=0;
+            }
         }
 };
