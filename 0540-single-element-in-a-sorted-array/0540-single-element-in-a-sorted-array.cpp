303ff0358f2c103fcc096134869bcf8a1a863cc2
Sparsh Jaggi <sparshjaggi2607@gmail.com>
2025-08-10 17:20:55 +0530

    Time: 0 ms (100%), Space: 26 MB (91.76%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -9,7 +9,7 @@ public:
         while(low<=high){
             int mid=(low+high)/2;
             if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1])return nums[mid];
-            if((mid%2==1 && nums[mid-1]==nums[mid])||(mid%2==0 && nums[mid]==nums[mid+1])low=mid+1;
+            if((mid%2==1 && nums[mid-1]==nums[mid])||(mid%2==0 && nums[mid]==nums[mid+1]))low=mid+1;
             else high=mid-1;
         }
         return -1;
